af7f2f03efc42a5e1694bec4a95821ef7e6d0a7b
Solarinayotomiwa <61776239+Solarinayotomiwa@users.noreply.github.com>
2025-06-18 23:32:04 +0100

    Added Newsltter

---
+++ src/components/NewsLetter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Title from './Title'
+
+const NewsLetter = () => {
+  return (
+    <div>
+        <div>
+            <Title title='Stay Inspired' subTitle='Join our newsletter and be the first to discover new destinations, exclusive offers, and travel inspiration.'/>
+        </div>
+    </div>
+  )
+}
+
+export default NewsLetter
